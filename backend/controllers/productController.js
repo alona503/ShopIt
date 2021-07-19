60dc6924dad9bbf2205135bf039dbd047877ddac
alona503 <alena.liamg@gmail.com>
2021-07-19 13:00:53 +0300

    delete product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../modules/product')
 
-//Create new product =>/api/v1/product/new
+//Create new product =>/api/v1/admin/product/new
 exports.newProduct = async (req, res, next) => {
   try {
     const product = await Product.create(req.body)
@@ -37,6 +37,47 @@ exports.getSingleProduct = async (req, res, next) => {
     res.status(200).json({
       success: true,
       product,
+    })
+  }
+}
+
+//update products =>/api/v1/admin/products/
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'product not found',
+    })
+  } else {
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    })
+
+    res.status(200).json({
+      success: true,
+      product,
+    })
+  }
+}
+
+//delete product =>/api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, nex) => {
+  const product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'product not found',
+    })
+  } else {
+    await product.remove()
+    res.status(200).json({
+      success: true,
+      message: 'Product deleted',
     })
   }
 }
