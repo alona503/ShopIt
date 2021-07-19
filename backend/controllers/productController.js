ceb9a592e7b68fbb759ce98a4546ffe0f6e284e3
alona503 <alena.liamg@gmail.com>
2021-07-19 21:02:15 +0300

    with error in getProducts

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,19 +1,16 @@
 const Product = require('../modules/product')
 const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncErrors = require('../middlewears/catchAsyncErrors')
 
 //Create new product =>/api/v1/admin/product/new
-exports.newProduct = async (req, res, next) => {
-  try {
-    const product = await Product.create(req.body)
+exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.create(req.body)
 
-    res.status(201).json({
-      success: true,
-      product,
-    })
-  } catch (error) {
-    console.log(error)
-  }
-}
+  res.status(201).json({
+    success: true,
+    product,
+  })
+})
 
 //get all products => /api/v1/products
 exports.getProducts = async (req, res, next) => {
