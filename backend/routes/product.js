2604ee5657ff202e977e09e0c6b8d2a65ec829a0
alona503 <alena.liamg@gmail.com>
2021-07-20 08:35:38 +0300

    / was missing in route now working

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -10,7 +10,7 @@ const {
 } = require('../controllers/productController')
 
 router.route('/products').get(getProducts)
-router.route('admin/product/new').post(newProduct)
+router.route('/admin/product/new').post(newProduct)
 router.route('/product/:id').get(getSingleProduct)
 router.route('/admin/product/:id').put(updateProduct).delete(deleteProduct)
 module.exports = router
